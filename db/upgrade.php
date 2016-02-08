1ac3d09ed37a5b9b88d35284b7e7dca578dce161
Bevan Holman <bevan@pukunui.com>
2016-02-08 17:09:55 +0800

    up version to upgrade db

---
+++ db/upgrade.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Custom authentication for WHIA project
+ *
+ * Upgrade script
+ *
+ * @package    auth_whia
+ * @author     Bevan Holman <bevan@pukunui.com>, Pukunui
+ * @copyright  2015 onwards, Pukunui
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+function xmldb_auth_whia_upgrade($oldversion) {
+    global $DB;
+    $dbman = $DB->get_manager();
+
+    /// Add a new table mdl_auth_rsa_cpdlog to plugin
+    if ($oldversion < 2016020802) {
+
+        // Define table auth_rsa_cpdlog to be created.
+        $table = new xmldb_table('auth_whia_domain');
+
+        // Adding fields to table auth_rsa_cpdlog.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '50', null, XMLDB_NOTNULL, null, null);
+
+
+        // Adding keys to table auth_rsa_cpdlog.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for auth_rsa_cpdlog.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Rsa savepoint reached.
+        upgrade_plugin_savepoint(true, 2016020801, 'auth', 'whia');
+    }
+    return true;
+}
--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2016020801;
+$plugin->version    = 2016020802;
 $plugin->requires   = 2015111602;
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component  = 'auth_whia';
