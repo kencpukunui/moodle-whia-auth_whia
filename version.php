040decc86c7dc7ba4973449185f196d454729099
Bevan Holman <bevan@pukunui.com>
2016-02-09 09:21:02 +0800

    update lang pack 2

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2016020804;
+$plugin->version    = 2016020805;
 $plugin->requires   = 2015111602;
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component  = 'auth_whia';
