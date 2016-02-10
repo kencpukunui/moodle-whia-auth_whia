2cfc4f4c3b95a739a5e754890778b52539c678a0
Bevan Holman <bevan@pukunui.com>
2016-02-10 09:24:27 +0800

    installing issue

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2016020814;
+$plugin->version    = 2016020815;
 $plugin->requires   = 2015111602;
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component  = 'auth_whia';
